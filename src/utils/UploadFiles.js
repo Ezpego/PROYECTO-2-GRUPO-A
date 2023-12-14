82d5f4fbaf0429db24754a0db22861d37e556198
Daniel Pardo Martin <daniel.pardo.martin20@gmail.com>
2023-12-14 18:01:11 +0100

    depuracion numero 1

---
+++ src/utils/UploadFiles.js
@@ -0,0 +1,16 @@
+import path from "path";
+import fileUpload from "express-fileupload";
+import crypto from "crypto";
+import { PUBLIC_DIR } from "../constants.js";
+import { SERVER_HOST } from "../constants.js";
+
+export async function subidaPrueba(photo, address) {
+    const fileExtension = path.extname(photo.name);
+    const randomFileName = crypto.randomUUID();
+    const newFilePath = `${randomFileName}${fileExtension}`;
+    const PHOTOS_DIR = path.join(PUBLIC_DIR, `${address}`);
+    await photo.mv(path.join(PHOTOS_DIR, newFilePath));
+
+    const URL = `${SERVER_HOST}/${address}/${newFilePath}`;
+    return URL;
+}
