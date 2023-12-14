bf5d06f38d8854216f63958375ea89f172bbd905
Gabriel Sastre <gabisas@icloud.com>
2023-12-14 11:33:43 +0100

    inclusion endpoints favoritos y opciones filtro

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,12 @@
 export const PORT = process.env.PORT || 3000;
 export const SERVER_HOST =
   process.env.SERVER_HOST || `http://localhost:${PORT}`;
+
+//-------------------
+// Añadido Gabriel
+//-------------------
+
+import path from "path";
+
+export const PUBLIC_DIR = path.join(process.cwd(), "public");
+export const PHOTOS_DIR = path.join(PUBLIC_DIR, "photos");
