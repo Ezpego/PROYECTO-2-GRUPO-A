518e96e444533f3e3dca8f6e6afd9cc45d01e2b5
Gabriel Sastre <gabisas@icloud.com>
2024-02-14 16:55:08 +0100

    para descartar

--- src/db/seed.js
+++ src/db/seed.js
@@ -7,41 +7,41 @@ console.log("prueba", process.env.MYSQL_DB);
 console.log("creando administrador");
 
 const generatePassword = async () => {
-    try {
-        const passAdmin = await bcrypt.hash(process.env.PASSWORD_ADMIN, 12);
-        return passAdmin;
-    } catch (error) {
-        console.error("Error al generar la contraseña:", error);
-        throw error;
-    }
+  try {
+    const passAdmin = await bcrypt.hash(process.env.PASSWORD_ADMIN, 12);
+    return passAdmin;
+  } catch (error) {
+    console.error("Error al generar la contraseña:", error);
+    throw error;
+  }
 };
 
 const main = async () => {
-    try {
-        const password = await generatePassword();
-        const emailAdmin = process.env.EMAIL_ADMIN;
-        const nameAdmin = process.env.NAME_ADMIN;
+  try {
+    const password = await generatePassword();
+    const emailAdmin = process.env.EMAIL_ADMIN;
+    const nameAdmin = process.env.NAME_ADMIN;
 
-        await db.query(
-            `INSERT INTO users (name, email, password, isAdministrator) VALUES("${nameAdmin}","${emailAdmin}","${password}",true)`
-        );
+    await db.query(
+      `INSERT INTO users (name, email, password, isAdministrator) VALUES("${nameAdmin}","${emailAdmin}","${password}",true)`
+    );
 
-        console.log("alimentando typology table");
+    console.log("alimentando typology table");
 
-        await db.query(
-            `INSERT INTO typology(name) VALUES ("cardio"),("fuerza"),("elasticidad"),("potencia"),("estabilidad"),("resistencia muscular"),("funcional"),("rehabilitacion"),("otros")`
-        );
+    await db.query(
+      `INSERT INTO typology(name) VALUES ("Cardio"),("Fuerza"),("Elasticidad"),("Potencia"),("Estabilidad"),("Resistencia Muscular"),("Funcional"),("Rehabilitacion"),("Otros")`
+    );
 
-        console.log("alimentando tabla muscle_group");
+    console.log("alimentando tabla muscle_group");
 
-        await db.query(
-            `INSERT INTO muscle_group(name) VALUES ("espalda"),("pecho"),("brazos"),("hombros"),("piernas"),("cuello"),("core")`
-        );
+    await db.query(
+      `INSERT INTO muscle_group(name) VALUES ("Espalda"),("Pecho"),("Brazos"),("Hombros"),("Piernas"),("Cuello"),("Core")`
+    );
 
-        await db.end();
-    } catch (error) {
-        console.error("Error durante la ejecución:", error);
-    }
+    await db.end();
+  } catch (error) {
+    console.error("Error durante la ejecución:", error);
+  }
 };
 
 main();
